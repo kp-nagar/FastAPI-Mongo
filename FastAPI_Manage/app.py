a3891c4e04c0a940e0ac084df59d67c171145918
kp-nagar <kumarpal.nagar@mindinventory.com>
2023-05-01 23:25:13 +0530

    add transaction code

--- FastAPI_Manage/app.py
+++ FastAPI_Manage/app.py
@@ -25,11 +25,39 @@ async def root():
 
 @app.get("/init")
 async def init_databse():
-    collection = app.mongodb_database.UserInformation
+    def callback(session, username, email, password, lastname=None):
+        user_data = {
+            'username': username,
+            'email': email,
+            'password': password,
+            'lastname': lastname
+        }
+        db = app.mongodb_database
+        user_collection = session.client.db.UserInformation
+
+        # Add new user data to 'UserInformation' collection
+        user_collection.insert_one(user_data, session=session)
+        print("Transaction successful")
+        return True
+    
+    def callback_wrapper(s):
+        callback(
+            s,
+            username = settings.ADMIN_NAME,
+            email = settings.ADMIN_EMAIL,
+            password = settings.ADMIN_PASSWD
+        )
+
+    with app.mongodb_client.start_session() as session:
+        session.with_transaction(callback_wrapper)
+    """
     admin_user = {
         'username': settings.ADMIN_NAME,
         'email': settings.ADMIN_EMAIL,
         'password': settings.ADMIN_PASSWD
     }
+    collection = app.mongodb_database.UserInformation
+    
     collection.insert_one(admin_user)
+    """
     return "Collection setup successfully."
