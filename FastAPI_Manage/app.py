9acc06285beea1d578cd1840fd74cd37fa211a20
kp-nagar <kumarpal.nagar@mindinventory.com>
2023-05-01 23:33:55 +0530

    add repository p.

--- FastAPI_Manage/app.py
+++ FastAPI_Manage/app.py
@@ -25,24 +25,12 @@ async def root():
 
 @app.get("/init")
 async def init_databse():
-    def callback(session, username, email, password, lastname=None):
-        user_data = {
-            'username': username,
-            'email': email,
-            'password': password,
-            'lastname': lastname
-        }
-        db = app.mongodb_database
-        user_collection = session.client.db.UserInformation
-
-        # Add new user data to 'UserInformation' collection
-        user_collection.insert_one(user_data, session=session)
-        print("Transaction successful")
-        return True
+    from src.api.v1 import repository
     
-    def callback_wrapper(s):
-        callback(
-            s,
+    def callback_wrapper(session):
+        repository.insert_user(
+            session,
+            app.mongodb_database,
             username = settings.ADMIN_NAME,
             email = settings.ADMIN_EMAIL,
             password = settings.ADMIN_PASSWD
@@ -50,14 +38,4 @@ async def init_databse():
 
     with app.mongodb_client.start_session() as session:
         session.with_transaction(callback_wrapper)
-    """
-    admin_user = {
-        'username': settings.ADMIN_NAME,
-        'email': settings.ADMIN_EMAIL,
-        'password': settings.ADMIN_PASSWD
-    }
-    collection = app.mongodb_database.UserInformation
-    
-    collection.insert_one(admin_user)
-    """
     return "Collection setup successfully."
---
+++ FastAPI_Manage/src/api/v1/repository.py
@@ -0,0 +1,13 @@
+def insert_user(session, db, username, email, password, lastname=None):
+    user_data = {
+        'username': username,
+        'email': email,
+        'password': password,
+        'lastname': lastname
+    }
+    user_collection = session.client.db.UserInformation
+
+    # Add new user data to 'UserInformation' collection
+    user_collection.insert_one(user_data, session=session)
+    print("Transaction successful")
+    return True
